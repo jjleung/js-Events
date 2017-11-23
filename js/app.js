9d12ffb8b3a77d4756116b18d834dc2406f364ff
Jesse J H Leung <Marduk@Jesses-MacBook-Air.local>
2017-11-23 12:03:42 -1000

    donezo

--- js/app.js
+++ js/app.js
@@ -6,15 +6,26 @@ The function will add the following content inside the paragraph with the id of 
 
 var bacon = "Shoulder turducken brisket, kevin swine andouille tri-tip salami tail ham sausage pork loin. Ribeye short loin rump kielbasa pork. Capicola short loin turducken corned beef tongue, chuck leberkas salami frankfurter. Kielbasa fatback pancetta, ground round meatball turducken jowl ribeye alcatra sirloin bacon corned beef beef ribs short loin. Pork belly spare ribs biltong corned beef meatball short ribs tongue alcatra swine drumstick. Biltong shankle kevin, cupim sirloin bresaola brisket. Tail pork belly biltong ball tip tri-tip, pig jerky cow pastrami prosciutto ;ground round bacon capicola tongue meatball.";
 
+function moreContent(){
+	document.getElementById('more').innerHTML = bacon;
+}
+
 
 //2. HTTP
 /*Create a function named `lessContent` that will initiate the `Show Less` link after clicking on it.
 
 The function will hide the contents in the pargraph with the id of `less` after clicking on the `Show Less` link.*/
 
+function lessContent(){
+	document.getElementById('less').innerHTML = "";
+}
 
 //3. Tacocat, The Original Palindrome King
 /*Create a function named `zoom` that will increase the font size of the paragraph with the id of `biggie` after hovering your mouse over it. Increae the font size to 150%*/
+
+function zoom(){
+	document.getElementById('biggie').style.fontSize = "150%";
+}
 
 
 //4. McDonalds
@@ -22,28 +33,75 @@ The function will hide the contents in the pargraph with the id of `less` after 
 
 Next, create a function named valueMenu that will display your favorite items in the paragraph the the id of `menu` after clicking on the showMenu paragraph.*/
 
+function valueMenu(){
+	document.getElementById('menu').innerHTML = '"Your favorite items."';
+}
 
 //5. Gin.
 /*Create a function named redFace that will change the paragraph text to red and a font size of 30px after clicking on the text.*/
 
+function redFace(){
+	var gin = document.getElementById("drink");
+	gin.style.color = "red";
+	gin.style.fontSize = "30px";
+}
 
 //6. Peanut Butter Cup Oreos
 /*Create a function `showPrice` that will add the price of `$5.55` inside the paragraph with the id `price` after hovering your mouse over the paragraph.*/
 
+function showPrice(){
+	document.getElementById("price").innerHTML = "$5.55";
+}
 
 //7. Mr. Buttons
 /*Add an Event Listener to the button that will display `myQuote` inside the paragraph with the id of `displayQuote` after the button is clicked.*/
 
 var myQuote = "Our lives are defined by opportunities; even the ones we miss.";
 
+function addQuote(){
+	document.getElementById("displayQuote").innerHTML = myQuote;
+}
+
+document.getElementById("Benjamin").addEventListener('click',addQuote);
 
 //8. Say It again, Randomly
 /*Create a function that will generate a random quote from the variable below after clicking on the button.*/
 
 var quotes = ["It's a funny thing about comin' home. Looks the same, smells the same, feels the same. You'll realize what's changed is you.", "Momma? Momma? Some days, I feel different than the day before.", "Some people, were born to sit by a river. Some get struck by lightning. Some have an ear for music. Some are artists. Some swim. Some know buttons. Some know Shakespeare. Some are mothers. And some people, dance.", "For what it's worth, it's never too late to be whoever you want to be."];
 
+function randQuote(){
+	var rInt = Math.floor(Math.random()*quotes.length);
+	document.getElementById("displayQuotes").innerHTML = quotes[rInt];
+}
 
+document.getElementById("random").addEventListener("click",randQuote);
 
 //9. Unlock the Secret to Financial Freedom
 /*Create an event listener that will show and hide the message when clicking on the button.
 */
+
+var theMoney = document.getElementById('showmoney').innerHTML;
+var buttonLoc = document.getElementById("showHide");
+
+
+var theFree = document.getElementById("freedom");
+var rMoney = document.createElement("div");
+rMoney.id = 'realMoney';
+buttonLoc.after(rMoney);
+
+
+var moneySwitch = 0;
+
+function moMoney(){
+
+	if (moneySwitch == 0){
+		document.getElementById('realMoney').innerHTML = theMoney;
+		moneySwitch++;
+	}
+	else if (moneySwitch == 1){
+		document.getElementById('realMoney').innerHTML = "";
+		moneySwitch--;
+	}
+}
+
+document.getElementById("showHide").addEventListener("click", moMoney);
